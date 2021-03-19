f00e40ce24f94abe046f5c2a5527fa8b76373af0
PTracz1996 <44437285+PTracz1996@users.noreply.github.com>
2021-03-19 10:17:46 +0100

    Update showClient.blade.php
    Zmiana sposoby wyswietlania klientów

--- resources/views/showClient.blade.php
+++ resources/views/showClient.blade.php
@@ -4,11 +4,28 @@
 
 <h1><center>Klienci</center></h1>
 <div class="list-group">
-  <a href="#" class="list-group-item list-group-item-action active"> Klient 1 123-456-789 Warszawa ul.Wiejska 12 </a>
-  <a href="#" class="list-group-item list-group-item-action">Klient 1 123-456-789 Warszawa ul.Wiejska 12</a>
-  <a href="#" class="list-group-item list-group-item-action">Klient 1 123-456-789 Warszawa ul.Wiejska 12</a>
-  <a href="#" class="list-group-item list-group-item-action">Klient 1 123-456-789 Warszawa ul.Wiejska 12</a>
-  <a href="#" class="list-group-item list-group-item-action ">Klient 1 123-456-789 Warszawa ul.Wiejska 12</a>
+  <ul class="list-group">
+  <li class="list-group-item">
+    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+    Klient 1 123-456-789 Warszawa ul.Wiejska 12
+  </li>
+  <li class="list-group-item">
+    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+    Klient 1 123-456-789 Warszawa ul.Wiejska 12
+  </li>
+  <li class="list-group-item">
+    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+    Klient 1 123-456-789 Warszawa ul.Wiejska 12
+  </li>
+  <li class="list-group-item">
+    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+    Klient 1 123-456-789 Warszawa ul.Wiejska 12
+  </li>
+  <li class="list-group-item">
+    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+    Klient 1 123-456-789 Warszawa ul.Wiejska 12
+  </li>
+</ul>
   <a class="btn btn-primary" href="/Create" role="button">Stwórz klienta</a>
 
 </div>
