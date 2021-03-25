4f59d81235a48b19ae2750f14422471a6c41b425
PTracz1996 <44437285+PTracz1996@users.noreply.github.com>
2021-03-25 23:02:46 +0100

    Poprawiony widok showClient
    Poprawki w widoku

--- resources/views/showClient.blade.php
+++ resources/views/showClient.blade.php
@@ -1,18 +1,44 @@
 @extends('welcome')
 
 @section('content')
+<style>
+.collapse{
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-family: 'Roboto', Arial, Tahoma, sans-serif;
+    margin: 0 0 5px;
+    line-height: 1.42857143;
+}
+.panel-danger{
+  font-family: 'Roboto', Arial, Tahoma, sans-serif;
+}
+</style>
 <h1><center>Klienci</center></h1>
 <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
 <div class="list-group">
 
   @foreach($clients as $client)
-  <button data-toggle="collapse" class="dataClick" data-id="{{$client->id}}">{{$client->company_name}}</button>
+  <button data-toggle="collapse" class="panel panel-danger" data-id="{{$client->id}}" >{{$client->company_name}} </button>
 
 <div id="{{$client->id}}" class="collapse">
-Lorem ipsum dolor text....
+<p><strong>Nazwa firmy :</strong> {{$client->company_name}} <br />
+<strong>NIP :</strong>{{$client->nip}} <br />
+<strong>E-mail :</strong> {{$client->email}} <br />
+<strong>Adres :</strong> ul. {{$client->street}} {{$client->house_number}}/{{$client->apartment_number}} , {{$client->post_code}} {{$client->city}} 
+<br />
+<strong>Numer telefonu :</strong> {{$client->phone_number}} </p> 
+<center >
+<a  href="#" ><button class="btn"><i class="fa fa-trash"></i></button></a>
+<a  href="#" ><button class="btn"><i class="fa fa-edit"></i></button></a>
+<a  href="#" ><button class="btn"><i class="fa fa-eye"></i></button></a>
+</center>
 </div>
     @endforeach
 
@@ -25,7 +51,7 @@ Lorem ipsum dolor text....
 @section('js-files')
     <script type="text/javascript">
 $(function() {
-    $('.dataClick').click(function() {
+    $('.panel-danger').click(function() {
       var id = $(this).data("id");
       var element = document.getElementById(id);
       if(element.style.display == "")
