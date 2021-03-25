aff4fc872a196b0cb4ab8b2b433de0ab1e82d8a4
kopecmarek <marek.kopecbg@gmail.com>
2021-03-25 21:52:44 +0100

    Added a JS script to switch display none/block

--- resources/views/showClient.blade.php
+++ resources/views/showClient.blade.php
@@ -1,50 +1,19 @@
 @extends('welcome')
 
 @section('content')
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 <h1><center>Klienci</center></h1>
-
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <div class="list-group">
-<<<<<<< HEAD
-<table class="table table-fixed table-responsive">
-  <thead>
-    <tr>
-      <th scope="col"><i class="fa fa-copyright"></i> Nazwa firmy</th>
-      <th scope="col"><i class="fa fa-indent"></i> NIP</th>
-      <th scope="col"><i class="fa fa-home"></i> Adres</th>
-      <th scope="col"><i class="fa fa-at"></i> E-mail</th>
-      <th scope="col"><i class="fa fa-phone"></i> Nr telefonu</th>
-      <th scope="col"><i class="fa fa-cog"></i> Akcja</th>
-    </tr>
-  </thead>
-  <tbody>
-  @foreach($clients as $client)
-    <tr class="clickableRow overflow-auto">
-      <td>{{$client->company_name}}</td>
-      <td>{{$client->nip}}</td>
-      <td>ul.{{@$client->street}}
-      {{@$client->house_number}}/{{@$client->apartment_number}}<br />
-      {{@$client->city}} {{@$client->post_code}}</td>
-      <td>{{$client->email}}</td>
-      <td>{{@$client->phone_number}}</td>
-      <td>
-      <button class="btn delete" data-id="{{$client->id}}"><i class="fa fa-trash"></i></button>
-      <a  href="/Edit" ><button class="btn" data-id="{{$client->id}}" data-token="{{ csrf_token() }}"><i class="fa fa-edit"></i></button></a>
-      <a  href="/client" ><button class="btn" data-id="{{$client->id}}"><i class="fa fa-eye"></i></button></a>
-      <a  href="/create/client" ><button class="btn" data-id="{{$client->id}}"><i class="fa fa-plus"></i></button></a>
-      </td>
-      </tr>
-=======
 
   @foreach($clients as $client)
-  <button data-toggle="collapse" data-target="'#{{$client->id}}">{{$client->company_name}}</button>
+  <button data-toggle="collapse" class="dataClick" data-id="{{$client->id}}">{{$client->company_name}}</button>
 
 <div id="{{$client->id}}" class="collapse">
 Lorem ipsum dolor text....
 </div>
->>>>>>> fca1c7065da4e8f701e8ace33f7559adf22ca5f0
     @endforeach
 
 {{ $clients->links() }}
@@ -54,5 +23,16 @@ Lorem ipsum dolor text....
     const deleteUrl = "{{ url('/client/') }}/";
 @endsection
 @section('js-files')
-    <script src="{{ asset('js/delete.js') }}"></script>
+    <script type="text/javascript">
+$(function() {
+    $('.dataClick').click(function() {
+      var id = $(this).data("id");
+      var element = document.getElementById(id);
+      if(element.style.display == "")
+      element.style.display = 'block';
+      else
+      element.style.display = '';
+    });
+});
+    </script>
 @endsection
